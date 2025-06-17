cb586ee7f178a194837336c15191cc135d812622
virtualized_human <92203938+virtualized-human@users.noreply.github.com>
2025-06-17 19:35:14 +0200

    Create cloudflare-import-ptr-gen.sh

---
+++ cloudflare-import-ptr-gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+file=$1
+output=$2
+touch $output
+truncate -s 0 $output
+echo ";; PTR Records" >> $output
+while IFS= read -r line; do
+    # Split the line on the keyword 'content'
+    ip="${line%%,*}"   # Everything before the first comma
+    name="${line#*,}"      # Everything after the first comma
+
+    echo "$(nslookup $ip | head -n 1 | awk '{print $1}'). 1 IN PTR $name. ; $ip" >> $output
+done < "$file"
